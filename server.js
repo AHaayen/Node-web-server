4782782f3de185d89adcfbf00286baa1b36cdafd
AndresHaayen <andreshaayen@gmail.com>
2017-10-26 16:42:21 +0200

    Learned how to get, send. Respond and Request. Print a string in a html LocalHost.

--- server.js
+++ server.js
@@ -3,7 +3,29 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    //res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Andres',
+        likes: [
+            'Programming',
+            'Games',
+            'Sport'
+        ]
+    });
 });
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+// /bad - send back json with errorMessage ('Error handeling request)
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error handeling request'
+    });
+});
+
 
 app.listen(3000);
