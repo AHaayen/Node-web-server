57cd5438da91308f492b4309c9b1a8a1fd1213eb
AndresHaayen <andreshaayen@gmail.com>
2017-10-28 20:23:36 +0200

    Learned new tricks with app.get , res.render , some registerHelper for hbs. and made another handlebar for Maintenance page with HTML code.

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
+
+// basic express middleware 
+app.use((req, res, next) => { // next exist so you can tell express when your middle ware function is done
+    // next(); >>>>  only way the app will continue to run
+    var now = new Date().toString();
+    var log = console.log(`${now}: ${req.method} ${req.url}`);
+
+    console.log(log);
+    fs.appendFile('server.log', log + `\n`, (err) => {
+        if(err){
+            console.log('Unable to append to server.log.');
+        };
+    });
+    next();
+});
+
+/*
+When you go to /help.html you will still get that page
+You won't get the maintenance page. 
+That is because middleware is excecuted in the order you call
+app.use
+*/
+
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); // This is a line of express middleware. 
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to dolois page'
+    });
+});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
